46f8b5680d7d1f7db3241cc05060ef98032951c4
nulman <nulman>
2016-12-25 17:16:40 +0200

    more milestone 3
    divided js files
    more changes to file explorer
    filter out .db files
    implemented loader wheel and fil icons

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,49 +1,6 @@
-var app = angular.module('store', [ ]);
-var currDir;
-var patt = /\\[^\\]+$/;
-
 $(document).ready(function() {
 
-  //treeScript();
-  //get root path
-  callUrl("");
   fixHeight();
-
-  
-
-  $('#jstree').bind("dblclick.jstree", function (event) {
-   var node = $(event.target).closest("li");
-   var url = node[0].id;
-   currDir = url
-   callUrlUpdate(url)
-
-  });
-
-  $('#up-dir').on('click',function(){
-    id = currDir.replace(patt, "");  
-    currDir = id;
-    callUrlUpdate(currDir + "\\");
-  });
-
-  //handles project selection
-  $('#proj-select').on('click', function () {
-    //get the path of the project
-    var node = $('#jstree').jstree('get_selected');
-    var id = node[0];
-
-    //if selection is valid (not a folder)
-    if(isValid(id)){
-      //get parameter list from the server, and fill parameters list
-      url = "/getcolumns?experiment="+id;
-      //$.get( url, fillProjectParameters);
-      $.ajax({
-        url: url,
-        success: fillProjectParameters,
-        error: serverErrorHandler
-      });
-    }
-  });
-
 
   //handles parameter selection
   $('#param-select').on('click', function () {
@@ -64,50 +21,10 @@ $(document).ready(function() {
 });
 
 
-//makes the request to the server with the given url
-//then draws the tree with the response json
-function callUrl(url){
-  $.ajax({
-        async : true,
-        type : "GET",
-        url : "/listdir?id=" + url,
-        dataType : "json",    
-
-        success : drawTreeWithJson      
-    });
-}
-
-function callUrlUpdate(url){
-  $.ajax({
-        async : true,
-        type : "GET",
-        url : "/listdir?id=" + url,
-        dataType : "json",    
-
-        success : updateTreeWithJson      
-    });
-}
-
-//checks that the selected id is indeed a file-type
-function isValid(id){
-  // var node = $('#jstree').find("[id="+id+"]")
-  // if(get_type(node) == "file"){
-  //   return true;
-  // }else{
-  //   return false;
-  // }
-  return true;
-}
-
-function updateTreeWithJson(data){
-  $('#jstree').jstree(true).settings.core.data = data;
-  $('#jstree').jstree(true).refresh();
-}
-
-
 function displayGraph(data){
   //hide error message (if present)
   $("#err-msg").addClass("hidden");
+  $("#graph-display").empty();
 
   var div = data.div;
   var js = data.js;
@@ -123,13 +40,14 @@ function displayGraph(data){
 }
 
 function serverErrorHandler(data){
+  $('#err-msg').append(data);
   $("#err-msg").removeClass("hidden");
 }
 
 //receives a json list of parameters and puts them in the x/y/machines parameters list
 function fillProjectParameters(data){
   //hide error message (if present)
-  $("#err-msg").addClass("hidden");
+  $("#err-msg").hide();
   //delete previous options information
   $("#x-axis").empty();
   $("#y-axis").empty();
@@ -145,125 +63,13 @@ function fillProjectParameters(data){
   })
 
   $('.nav-pills a[href="#parameter-selection"]').tab('show');
+
+  $('#loader').hide();
 }
 
 //fixes the height 
 function fixHeight(){
-  remaining_height = parseInt($(window).height() - 300); 
+  remaining_height = parseInt($(window).height() - 200); 
   $('#graph-display').height(remaining_height); 
 }
 
-function drawTree(url){
-  $(function() {
-      $('#jstree')
-        .jstree({
-          'core' : {
-            'data' : {
-              'url' : url,
-              "dataType" : "json",
-              'data' : function (node) {
-                return { 'id' : node.id };
-              }              
-            },
-            'themes' : {
-              "name": "default-dark",
-              "dots": true,
-              "icons": true,
-              'responsive' : false,
-              'variant' : 'small',
-              'stripes' : true
-            }
-          },
-          'sort' : function(a, b) {
-            return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
-          },
-          
-          'types' : {
-            'default' : { 'icon' : 'folder' },
-            'file' : { 'valid_children' : [], 'icon' : 'file' }
-          },
-          'unique' : {
-            'duplicate' : function (name, counter) {
-              return name + ' ' + counter;
-            }
-          },
-          'plugins' : ['state','dnd','sort','types','contextmenu','unique']
-        })
-      });
-}
-
-function drawTreeWithJson(json){
-  $(function() {
-      $('#jstree')
-        .jstree({
-          'core' : {
-            'data' : json,
-            'themes' : {
-              "name": "default-dark",
-              "dots": true,
-              "icons": true,
-              'responsive' : false,
-              'variant' : 'small',
-              'stripes' : true
-            }
-          },
-          'sort' : function(a, b) {
-            return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
-          },
-          
-          'types' : {
-            'default' : { 'icon' : 'folder' },
-            'file' : { 'valid_children' : [], 'icon' : 'file' }
-          },
-          'unique' : {
-            'duplicate' : function (name, counter) {
-              return name + ' ' + counter;
-            }
-          },
-          'plugins' : ['state','dnd','sort','types','contextmenu','unique']
-        })
-      });
-}
-
-
-//generates the file explorer tree 
-function treeScript(){
-
-    $(function() {
-      
-      var url = "/listdir"
-      $('#jstree')
-        .jstree({
-          'core' : {
-            'data' : {
-              'url' : url,
-              'data' : function (node) {
-                return { 'id' : node.id };
-              }
-            },
-            'themes' : {
-              "name": "default-dark",
-              "dots": true,
-              "icons": true,
-              'responsive' : false,
-              'variant' : 'small',
-              'stripes' : true
-            }
-          },
-          'sort' : function(a, b) {
-            return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
-          },
-          
-          'types' : {
-            'default' : { 'icon' : 'folder' },
-            'file' : { 'valid_children' : [], 'icon' : 'file' }
-          },
-          'unique' : {
-            'duplicate' : function (name, counter) {
-              return name + ' ' + counter;
-            }
-          },
-          'plugins' : ['state','dnd','sort','types','contextmenu','unique']
-        })
-      });
-}
---
+++ server/static/js/file-explorer.js
@@ -0,0 +1,141 @@
+// holds all the file-explorer creation and update functions
+
+
+var currDir;                //holds the current directory
+var patt = /\\[^\\]+$/;
+
+
+$(document).ready(function() {
+
+
+  $('#loader').hide();
+  //get root path
+  callUrl("");
+  //fixes explorer height to fill whole screen
+  maxHeight('#file-explorer');
+
+ 
+  //handles file explorer double-click event
+  $('#jstree').bind("dblclick.jstree", function (event) {
+   //var node = $(event.target).closest("li");
+   var node = $('#jstree').jstree('get_selected',true);
+   //if folder - update tree
+   if(node[0].type == 'default'){
+   	var url = node[0].id;
+   	currDir = url;
+   	callUrlUpdate(url);
+   }else{
+   	//do nothing - file is selected
+   }
+  });
+
+  //handles file explorer 'up directory' button
+  $('#up-dir').on('click',function(){
+    id = currDir.replace(patt, "");  
+    currDir = id;
+    callUrlUpdate(currDir + "\\");
+  });
+
+  //handles project selection- 'accept' button
+  $('#proj-select').on('click', function () {
+    //get the path of the project
+    var node = $('#jstree').jstree('get_selected',true);
+    var id = node[0].id;
+
+    //if selection is valid (not a folder)
+    if(isValid(node[0])){
+      //get parameter list from the server, and fill parameters list
+      url = "/getcolumns?experiment="+id;
+      $('#loader').show();
+
+      $.ajax({
+        url: url,
+        success: fillProjectParameters,
+        error: serverErrorHandler
+      });
+    }
+  });
+});
+
+
+//makes the request to the server with the given url
+//then draws the tree with the response json
+function callUrl(url){
+  $.ajax({
+        aync : true,
+        type : "GET",
+        url : "/listdir?id=" + url,
+        dataType : "json",    
+
+        success : drawTree      
+    });
+}
+
+//makes the request to the server with the given url
+//then updates the existing tree with the response json
+function callUrlUpdate(url){
+  $.ajax({
+        async : true,
+        type : "GET",
+        url : "/listdir?id=" + url,
+        dataType : "json",    
+
+        success : updateTree      
+    });
+}
+
+//checks that the selected id is indeed a file-type
+function isValid(node){
+  if(node.type == 'file'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+//updates the tree with new data
+function updateTree(data){
+  $('#jstree').jstree(true).settings.core.data = data;
+  $('#jstree').jstree(true).refresh();
+}
+
+//draws the initial tree
+function drawTree(json){
+  $(function() {
+      $('#jstree')
+        .jstree({
+          'core' : {
+            'data' : json,
+            'themes' : {
+              "name": "default-dark",
+              //"dots": true,
+              "icons": true,
+              'responsive' : false,
+              //'variant' : 'small',
+              'stripes' : true
+            }
+          },
+          'sort' : function(a, b) {
+            return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
+          },
+          
+          'types' : {
+            'default' : { 'icon' : 'glyphicon glyphicon-folder' },
+            'file' : { 'valid_children' : [], 'icon' : 'glyphicon glyphicon-file' }
+          },
+          'unique' : {
+            'duplicate' : function (name, counter) {
+              return name + ' ' + counter;
+            }
+          },
+          'plugins' : ['state','dnd','sort','types','contextmenu','unique']
+        })
+      });
+}
+
+//fixes the height 
+function maxHeight(div){
+	var header_height = 100;
+	var remaining_height = parseInt($(window).height() - header_height); 
+	$(div).height(remaining_height); 
+}
